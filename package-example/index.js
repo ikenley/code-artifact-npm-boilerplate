4c3c8d1acf743b474135747a1d3f120566a63f5c
ikenley <ikenley6@gmail.com>
2022-09-01 09:47:05 -0400

    init (#1)

---
+++ package-example/index.js
@@ -0,0 +1,9 @@
+export const sayHello = (name) => {
+    console.log(`Open the pod bay doors, ${name}`);
+}
+
+export const respond = (name) => {
+    console.log(`I'm afraid I can't let you do that, ${name}`);
+}
+
+export default sayHello;
